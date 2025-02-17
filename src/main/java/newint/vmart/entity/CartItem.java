f98fc50a8344254a36c4c0e75e1abf0bf7a0a62d
Long Nguyen <longnewint@gmail.com>
2025-02-17 18:01:32 -0500

    Change CartItem from record to class

--- src/main/java/newint/vmart/entity/CartItem.java
+++ src/main/java/newint/vmart/entity/CartItem.java
@@ -1,4 +1,11 @@
 package newint.vmart.entity;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 public class CartItem {
+  @NotBlank(message = "productId cannot be blank")
+  public String productId;
+  public int quantity;
 }
