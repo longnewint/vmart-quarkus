4089d41b138f4ddd08c6a6e4449f950daa1eed97
Long Nguyen <longnewint@gmail.com>
2025-02-18 13:03:01 -0500

    Cascading name change fix

--- src/main/java/newint/vmart/api/CategoryApi.java
+++ src/main/java/newint/vmart/api/CategoryApi.java
@@ -4,7 +4,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import newint.vmart.data.CategoryRepo;
-import newint.vmart.entity.ProductCategoryView;
+import newint.vmart.entity.ProductCategory;
 import org.jboss.resteasy.reactive.RestPath;
 
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ public class CategoryApi {
   CategoryRepo repo;
 
   @GET
-  public ArrayList<ProductCategoryView> get(@RestPath int categoryId) {
+  public ArrayList<ProductCategory> get(@RestPath int categoryId) {
     return repo.getProductByCategory(101, categoryId);
   }
 }
--- src/main/java/newint/vmart/data/CategoryRepo.java
+++ src/main/java/newint/vmart/data/CategoryRepo.java
@@ -3,7 +3,7 @@ package newint.vmart.data;
 import io.agroal.api.AgroalDataSource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import newint.vmart.entity.ProductCategoryView;
+import newint.vmart.entity.ProductCategory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,8 +18,8 @@ public class CategoryRepo {
 
   private static final String SQL = "SELECT * FROM get_product_view(?, ?)";
 
-  public ArrayList<ProductCategoryView> getProductByCategory(int storeId, int categoryId) {
-    ArrayList<ProductCategoryView> products = new ArrayList<>();
+  public ArrayList<ProductCategory> getProductByCategory(int storeId, int categoryId) {
+    ArrayList<ProductCategory> products = new ArrayList<>();
 
     try (Connection connection = this.pool.getConnection()) {
       try(PreparedStatement stm = connection.prepareStatement(SQL)) {
@@ -28,7 +28,7 @@ public class CategoryRepo {
 
         try(ResultSet rs = stm.executeQuery()) {
           while(rs.next()) {
-            products.add(new ProductCategoryView(
+            products.add(new ProductCategory(
               rs.getString(1),
               rs.getString(2),
               rs.getString(3),
