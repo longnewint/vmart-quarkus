82f1685f4a44820d4267108dab110fd5e6f8bb3a
Long Nguyen <longnewint@gmail.com>
2025-02-23 23:57:07 -0500

    Add product select by category to ProductRepo

--- src/main/java/newint/vmart/api/ProductApi.java
+++ src/main/java/newint/vmart/api/ProductApi.java
@@ -3,7 +3,6 @@ package newint.vmart.api;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import newint.vmart.data.CategoryRepo;
 import newint.vmart.data.ProductRepo;
 import newint.vmart.entity.Product;
 import newint.vmart.entity.ProductCategory;
@@ -17,18 +16,15 @@ import java.util.Optional;
 public class ProductApi {
   @Inject ProductRepo repo;
 
-  @Inject
-  CategoryRepo other;
-
   @GET
   @Path("/c/{categoryId}")
-  public List<ProductCategory> getByCategory(@RestPath int categoryId, @RestQuery int storeId)) {
-    return other.getProduct(101, categoryId);
+  public List<ProductCategory> getByCategory(@RestPath int categoryId, @RestQuery int storeId) {
+    return repo.getProductByCategory(storeId, categoryId);
   }
 
   @GET
   @Path("/{productId}")
   public Optional<Product> getById(@RestPath int productId, @RestQuery int storeId) {
-    return repo.getProduct(storeId, productId);
+    return repo.getProductById(storeId, productId);
   }
 }
--- src/main/java/newint/vmart/data/ProductRepo.java
+++ src/main/java/newint/vmart/data/ProductRepo.java
@@ -3,13 +3,17 @@ package newint.vmart.data;
 import io.agroal.api.AgroalDataSource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import newint.vmart.data.mapper.ProductCategoryMapper;
 import newint.vmart.data.mapper.ProductMapper;
 import newint.vmart.entity.Product;
+import newint.vmart.entity.ProductCategory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,11 +21,37 @@ import java.util.Optional;
 public class ProductRepo {
   @Inject AgroalDataSource pool;
 
-  private static final String SELECT_QUERY = "SELECT * FROM get_product_by_id(?, ?)";
+  private static final String SELECT_BY_CATEGORY_QUERY = "SELECT * FROM get_product(?, ?)";
+  private static final String SELECT_BY_ID_QUERY = "SELECT * FROM get_product_by_id(?, ?)";
 
-  public Optional<Product> getProduct(int storeId, int productId) {
+  public List<ProductCategory> getProductByCategory(int storeId, int categoryId) {
+    List<ProductCategory> products = Collections.emptyList();
+
+    try (Connection connection = this.pool.getConnection()) {
+      try(PreparedStatement stm = connection.prepareStatement(SELECT_BY_CATEGORY_QUERY)) {
+        stm.setInt(1, storeId);
+        stm.setInt(2, categoryId);
+
+        try(ResultSet rs = stm.executeQuery()) {
+          products = new ArrayList<>();
+          var mapper = new ProductCategoryMapper();
+
+          while(rs.next()) {
+            products.add(mapper.map(rs));
+          }
+          return products;
+        }
+      }
+    }
+    catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+  public Optional<Product> getProductById(int storeId, int productId) {
     try(Connection connection = this.pool.getConnection()) {
-      try(PreparedStatement stm = connection.prepareStatement(SELECT_QUERY)) {
+      try(PreparedStatement stm = connection.prepareStatement(SELECT_BY_ID_QUERY)) {
         stm.setInt(1, storeId);
         stm.setInt(2, productId);
 
