0319963982908dcb45ace32d02e3678f1b70002a
Long Nguyen <longnewint@gmail.com>
2025-02-24 14:34:37 -0500

    Add OrderApi

--- src/main/java/newint/vmart/api/OrderApi.java
+++ src/main/java/newint/vmart/api/OrderApi.java
@@ -1,4 +1,21 @@
 package newint.vmart.api;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import newint.vmart.data.OrderRepo;
+import newint.vmart.entity.CurrentOrder;
+import org.jboss.resteasy.reactive.RestPath;
+
+import java.util.List;
+
+@Path("/order")
 public class OrderApi {
+  @Inject OrderRepo repo;
+
+  @GET
+  @Path("/current/{userId}")
+  public List<CurrentOrder> getCurrentOrder(@RestPath int userId) {
+    return repo.getCurrentOrder(userId);
+  }
 }
