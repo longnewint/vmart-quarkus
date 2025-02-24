33cd8abf8f4ee29cbbe37799c8bf7dab114583ac
Long Nguyen <longnewint@gmail.com>
2025-02-24 14:34:07 -0500

    Add CurrentOrderMapper

--- src/main/java/newint/vmart/data/mapper/CurrentOrderMapper.java
+++ src/main/java/newint/vmart/data/mapper/CurrentOrderMapper.java
@@ -1,4 +1,49 @@
 package newint.vmart.data.mapper;
 
-public class CurrentOrderMapper {
+import newint.vmart.entity.CurrentOrder;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CurrentOrderMapper implements ResultMapper<CurrentOrder> {
+  public CurrentOrder map(final ResultSet rs) throws SQLException {
+    return new CurrentOrder(
+      rs.getInt(1),
+      rs.getString(2),
+      rs.getInt(3),
+      rs.getTimestamp(4),
+      rs.getFloat(5),
+      rs.getInt(6),
+      formatAddress(
+        rs.getString(7),
+        rs.getString(8),
+        rs.getString(9),
+        rs.getString(10),
+        rs.getString(11),
+        rs.getString(12),
+        rs.getString(13)
+      )
+    );
+  }
+
+  public String formatAddress(
+    String unitNumber,
+    String streetNumber,
+    String addressLine1,
+    String addressLine2,
+    String postalCode,
+    String city,
+    String province) {
+    String tmpUnitNumber = !unitNumber.equals("none") ? unitNumber + ", " : "";
+    String tmpAddressLine2 = !addressLine2.equals("none") ? addressLine2 + ", " : "";
+
+    return String.format("%s%s %s, %s%s, %s %s",
+      tmpUnitNumber,
+      streetNumber,
+      addressLine1,
+      tmpAddressLine2,
+      city,
+      province,
+      postalCode);
+  }
 }
