f98f52eeab96080e600a6781407508f4f78b52e0
Long Nguyen <longnewint@gmail.com>
2025-02-24 14:34:28 -0500

    Add OrderRepo

--- src/main/java/newint/vmart/data/OrderRepo.java
+++ src/main/java/newint/vmart/data/OrderRepo.java
@@ -1,4 +1,43 @@
 package newint.vmart.data;
 
+import io.agroal.api.AgroalDataSource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import newint.vmart.data.mapper.CurrentOrderMapper;
+import newint.vmart.entity.CurrentOrder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@ApplicationScoped
 public class OrderRepo {
+  @Inject AgroalDataSource pool;
+
+  private static final String SELECT_CURRENT_ORDER = "SELECT * FROM get_current_order(?)";
+
+  public List<CurrentOrder> getCurrentOrder(int userId) {
+    try(Connection connection = pool.getConnection()) {
+      try(PreparedStatement ps = connection.prepareStatement(SELECT_CURRENT_ORDER)) {
+        ps.setInt(1, userId);
+
+        try(ResultSet rs = ps.executeQuery()) {
+          List<CurrentOrder> currentOrders = new ArrayList<>();
+          CurrentOrderMapper mapper = new CurrentOrderMapper();
+
+          while(rs.next()) {
+            currentOrders.add(mapper.map(rs));
+          }
+
+          return currentOrders;
+        }
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
