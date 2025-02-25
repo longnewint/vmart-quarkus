60e256a5a19a9caf68c4ae940aebd9b2e6ce4045
Long Nguyen <longnewint@gmail.com>
2025-02-25 15:10:28 -0500

    Add addAddress function to AddressRepo

--- src/main/java/newint/vmart/data/AddressRepo.java
+++ src/main/java/newint/vmart/data/AddressRepo.java
@@ -5,13 +5,11 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import newint.vmart.data.mapper.AddressMapper;
 import newint.vmart.data.mapper.CartItemMapper;
-import newint.vmart.entity.Address;
+import newint.vmart.entity.AddressRead;
+import newint.vmart.entity.AddressWrite;
 import newint.vmart.entity.CartItemRead;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,13 +19,14 @@ public class AddressRepo {
   @Inject
   AgroalDataSource pool;
 
-  private static final String READ_QUERY = "SELECT * FROM get_address(?)";
+  private static final String SELECT_QUERY = "SELECT * FROM get_address(?)";
+  private static final String INSERT_QUERY = "CALL add_address(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-  public List<Address> getAddress(int userId) {
-    List<Address> addresses = Collections.emptyList();
+  public List<AddressRead> getAddress(int userId) {
+    List<AddressRead> addresses = Collections.emptyList();
 
     try(Connection connection = this.pool.getConnection()) {
-      try(PreparedStatement stm = connection.prepareStatement(READ_QUERY)) {
+      try(PreparedStatement stm = connection.prepareStatement(SELECT_QUERY)) {
         stm.setInt(1, userId);
 
         try(ResultSet rs = stm.executeQuery()) {
@@ -39,6 +38,32 @@ public class AddressRepo {
           }
           return addresses;
         }
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public boolean addAddress(int userId, AddressWrite address) {
+    try(Connection connection = pool.getConnection()) {
+      try(CallableStatement stm = connection.prepareCall(INSERT_QUERY)) {
+        stm.setInt(1, userId);
+        stm.setBoolean(2, false);
+
+        String unitNumber = address.unitNumber() == null ? "none" : address.unitNumber();
+        stm.setString(3, unitNumber);
+
+        stm.setString(4, address.streetNumber());
+        stm.setString(5, address.addressLine1());
+
+        String addressLine2 = address.addressLine2() == null ? "none" : address.addressLine2();
+        stm.setString(6, addressLine2);
+
+        stm.setString(7, address.city());
+        stm.setString(8, address.province());
+        stm.setString(9, address.postalCode());
+
+        return stm.execute();
       }
     } catch (SQLException e) {
       throw new RuntimeException(e);
