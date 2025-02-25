af4765ed7f28f9f67a7aa225887119be43d4995f
Long Nguyen <longnewint@gmail.com>
2025-02-25 15:10:46 -0500

    Add post api to DeliveryApi

--- src/main/java/newint/vmart/api/DeliveryApi.java
+++ src/main/java/newint/vmart/api/DeliveryApi.java
@@ -1,20 +1,44 @@
 package newint.vmart.api;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import newint.vmart.api.validation.Result;
 import newint.vmart.data.AddressRepo;
-import newint.vmart.entity.Address;
+import newint.vmart.entity.AddressRead;
+import newint.vmart.entity.AddressWrite;
 import org.jboss.resteasy.reactive.RestPath;
 
 import java.util.List;
+import java.util.Set;
 
 @Path("/delivery/{userId}")
 public class DeliveryApi {
   @Inject AddressRepo repo;
 
+  @Inject Validator validator;
+
   @GET
-  public List<Address> get(@RestPath int userId) {
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<AddressRead> get(@RestPath int userId) {
     return repo.getAddress(userId);
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Result post(@RestPath int userId, @Valid AddressWrite address) {
+    Set<ConstraintViolation<AddressWrite>> violations = validator.validate(address);
+
+    if(violations.isEmpty()) {
+      repo.addAddress(userId, address);
+
+      return new Result("Operation successful!");
+    }
+    else
+      return new Result(violations);
   }
 }
