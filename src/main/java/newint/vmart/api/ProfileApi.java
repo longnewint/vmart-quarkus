4e12b19d37ce3df377011a7026c6199f78a96e76
Long Nguyen <longnewint@gmail.com>
2025-02-25 23:55:08 -0500

    Add profile patch in ProfileApi

--- src/main/java/newint/vmart/api/ProfileApi.java
+++ src/main/java/newint/vmart/api/ProfileApi.java
@@ -1,17 +1,25 @@
 package newint.vmart.api;
 
 import jakarta.inject.Inject;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import newint.vmart.api.validation.Result;
 import newint.vmart.data.ProfileRepo;
 import newint.vmart.entity.ProfileRead;
+import newint.vmart.entity.ProfileWrite;
 import org.jboss.resteasy.reactive.RestPath;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Path("/profile")
 public class ProfileApi {
   @Inject ProfileRepo repo;
+
+  @Inject Validator validator;
 
   @GET
   @Path("/{userId}")
@@ -24,5 +32,15 @@ public class ProfileApi {
   @Path("/{userId}")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  public Result
+  public Result patch(@RestPath int userId, @Valid ProfileWrite profile) {
+    Set<ConstraintViolation<ProfileWrite>> violations = validator.validate(profile);
+
+    if(violations.isEmpty()) {
+      repo.updateProfile(userId, profile);
+
+      return new Result(true, "Operation successful!");
+    }
+    else
+      return new Result(violations);
+  }
 }
