f02613753e6145768bb98191703b0a6c19fd5003
Long Nguyen <longnewint@gmail.com>
2025-02-25 20:02:17 -0500

    Add post method to PaymentApi

--- src/main/java/newint/vmart/api/PaymentApi.java
+++ src/main/java/newint/vmart/api/PaymentApi.java
@@ -1,20 +1,44 @@
 package newint.vmart.api;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import newint.vmart.api.validation.Result;
 import newint.vmart.data.PaymentRepo;
 import newint.vmart.entity.PaymentRead;
+import newint.vmart.entity.PaymentWrite;
 import org.jboss.resteasy.reactive.RestPath;
 
 import java.util.List;
+import java.util.Set;
 
 @Path("/payment/{userId}")
 public class PaymentApi {
   @Inject PaymentRepo repo;
 
+  @Inject Validator validator;
+
   @GET
+  @Produces(MediaType.APPLICATION_JSON)
   public List<PaymentRead> get(@RestPath int userId) {
     return repo.getPayment(userId);
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Result post(@RestPath int userId, @Valid PaymentWrite payment) {
+    Set<ConstraintViolation<PaymentWrite>> violations = validator.validate(payment);
+
+    if(violations.isEmpty()) {
+      repo.addPayment(payment);
+
+      return new Result(true, "Operation successful!");
+    }
+    else
+      return new Result(violations);
   }
 }
