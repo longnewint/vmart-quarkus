41dc3e68070955c490bfdffcffb79db3fb069a24
Long Nguyen <longnewint@gmail.com>
2025-02-26 21:42:22 -0500

    Add result to add payment method

--- src/main/java/newint/vmart/api/PaymentApi.java
+++ src/main/java/newint/vmart/api/PaymentApi.java
@@ -34,7 +34,7 @@ public class PaymentApi {
     Set<ConstraintViolation<PaymentWrite>> violations = validator.validate(payment);
 
     if(violations.isEmpty()) {
-      repo.addPayment(userId, payment);
+      boolean isSuccessful = repo.addPayment(userId, payment);
 
       return new Result(true, "Operation successful!");
     }
