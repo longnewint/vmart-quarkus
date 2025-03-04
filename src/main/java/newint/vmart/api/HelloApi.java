8d8db19cd274f93074ba0aa04a5b62c352bd1fab
Long Nguyen <longnewint@gmail.com>
2025-03-03 22:43:47 -0500

    Add HelloApi

---
+++ src/main/java/newint/vmart/api/HelloApi.java
@@ -0,0 +1,15 @@
+package newint.vmart.api;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloApi {
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String get() {
+    return "Hello from Duke on Quarkus 3.15!";
+  }
+}
