42f56f7fd90952e33c9b70207590d3f6fac05426
Long Nguyen <longnewint@gmail.com>
2025-05-02 19:06:27 -0400

    Add fields to CartItemRead

--- src/main/java/newint/vmart/entity/CartItemRead.java
+++ src/main/java/newint/vmart/entity/CartItemRead.java
@@ -2,8 +2,12 @@ package newint.vmart.entity;
 
 public record CartItemRead(
   int productId,
+  String brand,
   String productName,
   float listPrice,
+  float discount_price,
+  String package_size,
+  String unit_price,
   String thumbnailUrl,
   int quantity
 ) {}
