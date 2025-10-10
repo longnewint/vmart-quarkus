493e31e3389cbaa03174261a5aeb6ba908195abb
Long Nguyen <longnewint@gmail.com>
2025-10-10 10:11:59 -0400

    c productmapper

--- src/main/java/newint/vmart/data/mapper/ProductMapper.java
+++ src/main/java/newint/vmart/data/mapper/ProductMapper.java
@@ -17,8 +17,7 @@ public class ProductMapper implements ResultMapper<Product> {
       rs.getString(7),
       rs.getString(8),
       rs.getString(9),
-      rs.getString(10),
-      rs.getString(11)
+      rs.getString(10)
     );
   }
 }
