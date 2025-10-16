d4134484db706d14ece7c4832bfd09f492692d03
Long Nguyen <longnewint@gmail.com>
2025-10-16 09:54:53 -0400

    add PaymentRead

--- src/main/java/newint/vmart/entity/PaymentRead.java
+++ src/main/java/newint/vmart/entity/PaymentRead.java
@@ -2,7 +2,6 @@ package newint.vmart.entity;
 
 public record PaymentRead(
   int paymentMethodId,
-  int paymentType,
-  String cardNumber,
-  boolean isDefault
+  int paymentTypeId,
+  String cardNumber
 ) {}
