2908a8acbe0e45871748b78a5a33b3b1052ea2bc
Long Nguyen <longnewint@gmail.com>
2025-10-20 23:56:34 -0400

    laksfdjl

--- src/main/java/newint/vmart/api/ProductApi.java
+++ src/main/java/newint/vmart/api/ProductApi.java
@@ -6,8 +6,8 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import newint.vmart.data.ProductRepo;
-import newint.vmart.entity.Product;
-import newint.vmart.entity.ProductCategory;
+import newint.vmart.entity.ProductIdRead;
+import newint.vmart.entity.ProductRead;
 import org.jboss.resteasy.reactive.RestPath;
 import org.jboss.resteasy.reactive.RestQuery;
 
@@ -21,14 +21,14 @@ public class ProductApi {
   @GET
   @Path("/c/{categoryId}")
   @Produces(MediaType.APPLICATION_JSON)
-  public List<ProductCategory> getByCategory(@RestPath int categoryId, @RestQuery int storeId) {
-    return repo.getProductByCategory(storeId, categoryId);
+  public List<ProductRead> get(@RestPath int categoryId, @RestQuery int storeId) {
+    return repo.getProduct(storeId, categoryId);
   }
 
   @GET
   @Path("/{productId}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Optional<Product> getById(@RestPath int productId, @RestQuery int storeId) {
+  public Optional<ProductIdRead> getById(@RestPath int productId, @RestQuery int storeId) {
     return repo.getProductById(storeId, productId);
   }
 }
