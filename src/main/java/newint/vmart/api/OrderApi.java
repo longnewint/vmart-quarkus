596216bdd70ced45b7975321a90cf094cf3d1b5a
Long Nguyen <longnewint@gmail.com>
2025-10-20 23:56:10 -0400

    akljfds

--- src/main/java/newint/vmart/api/OrderApi.java
+++ src/main/java/newint/vmart/api/OrderApi.java
@@ -4,28 +4,36 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import newint.vmart.data.OrderRepo;
+import newint.vmart.entity.OrderIdRead;
 import newint.vmart.entity.OrderRead;
 import newint.vmart.entity.OrderWrite;
 import org.jboss.resteasy.reactive.RestPath;
 
 import java.util.List;
+import java.util.Optional;
 
 @Path("/order")
 public class OrderApi {
   @Inject OrderRepo repo;
 
   @GET
-  @Path("/{userId}")
   @Produces(MediaType.APPLICATION_JSON)
   public List<OrderRead> getOrder(@RestPath int userId) {
-    return repo.getOrder(userId);
+    return repo.getOrder(12321);
+  }
+
+  @GET
+  @Path("/{orderId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Optional<OrderIdRead> getOrderById(@RestPath int orderId) {
+    return repo.getOrderById(orderId);
   }
 
   @POST
-  @Path("/{userId}")
+  @Path("/")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  public void post(@RestPath int userId, OrderWrite order) {
-    boolean isSuccessful = repo.addOrder(userId, order);
+  public void post(OrderWrite order) {
+    boolean isSuccessful = repo.addOrder(12321, order);
   }
 }
