604fd42bfb87efbfcb9b8646c3772dfc6cd398ab
Long Nguyen <longnewint@gmail.com>
2025-12-22 11:46:19 -0500

    c OrderRepo

--- src/main/java/newint/vmart/data/OrderRepo.java
+++ src/main/java/newint/vmart/data/OrderRepo.java
@@ -3,34 +3,59 @@ package newint.vmart.data;
 import io.agroal.api.AgroalDataSource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import newint.vmart.data.mapper.OrderIdReadMapper;
 import newint.vmart.data.mapper.OrderReadMapper;
+import newint.vmart.entity.OrderIdRead;
 import newint.vmart.entity.OrderRead;
 import newint.vmart.entity.OrderWrite;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class OrderRepo {
   @Inject AgroalDataSource pool;
 
-  private static final String SELECT_CURRENT_ORDER_QUERY = "SELECT * FROM get_order(?)";
+  private static final String GET_ORDER_QUERY = "SELECT * FROM get_order(?)";
+  private static final String GET_ORDER_BY_ID_QUERY = "SELECT * FROM get_order_by_id(?)";
   private static final String INSERT_QUERY = "CALL create_order(?, ?, ?, ?, ?, ?)";
+
+  public Optional<OrderIdRead> getOrderById(int orderId) {
+    try(Connection connection = this.pool.getConnection()) {
+      try(PreparedStatement ps = connection.prepareStatement(GET_ORDER_BY_ID_QUERY)) {
+        ps.setInt(1, orderId);
+
+        try(ResultSet rs = ps.executeQuery()) {
+
+          if(rs.next()) {
+            var mapper = new OrderIdReadMapper();
+            return Optional.ofNullable(mapper.map(rs));
+          }
+          else
+            return Optional.empty();
+        }
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   public List<OrderRead> getOrder(int userId) {
     try(Connection connection = this.pool.getConnection()) {
-      try(PreparedStatement ps = connection.prepareStatement(SELECT_CURRENT_ORDER_QUERY)) {
+      try(PreparedStatement ps = connection.prepareStatement(GET_ORDER_QUERY)) {
         ps.setInt(1, userId);
 
         try(ResultSet rs = ps.executeQuery()) {
-          List<OrderRead> currentOrders = new ArrayList<>();
+          List<OrderRead> orders = new ArrayList<>();
           var mapper = new OrderReadMapper();
 
           while(rs.next()) {
-            currentOrders.add(mapper.map(rs));
+            orders.add(mapper.map(rs));
           }
 
-          return currentOrders;
+          return orders;
         }
       }
     } catch (SQLException e) {
